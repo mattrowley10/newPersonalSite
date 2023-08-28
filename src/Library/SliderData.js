d6287f7d5f558e5be258c3fe8cce947ae293cf10
Matthew Rowley <mattrowley10@gmail.com>
2023-08-28 15:59:27 -0600

    removedPics

--- src/Library/SliderData.js
+++ src/Library/SliderData.js
@@ -1,37 +1,28 @@
 export const SliderData = [
   {
     image:
-      "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/326662896_1391496918256470_472247727960307476_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=hU6fezjDjWUAX8ptH-F&_nc_ht=scontent-den4-1.xx&oh=00_AfAnUeAnjGWHPEe8weIHT6IwKvy9jDubLfiq59_NCU0dIg&oe=64E63294&auto=format&fit=crop&w=1170&q=80",
+      "https://scontent-den4-1.xx.fbcdn.net/v/t1.6435-9/134174951_3880489448636127_5629388319572094141_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=0JcilSE31y4AX8DbXn-&_nc_ht=scontent-den4-1.xx&oh=00_AfDbXsAPj8V6xaB1UefgI_ETJuMOGrO6pI2g1rTVMHcAdQ&oe=65036A49&auto=format&fit=crop&w=1170&q=80",
     id: 1,
   },
+
   {
-    image:
-      "https://scontent-den4-1.xx.fbcdn.net/v/t1.6435-9/134174951_3880489448636127_5629388319572094141_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=0JcilSE31y4AX8DbXn-&_nc_ht=scontent-den4-1.xx&oh=00_AfDbXsAPj8V6xaB1UefgI_ETJuMOGrO6pI2g1rTVMHcAdQ&oe=65036A49&auto=format&fit=crop&w=1170&q=80",
+    image: "Images/mattPic1.jpg",
     id: 2,
   },
   {
-    image:
-      "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/365194130_6811801915504851_3369743226334341257_n.jpg?stp=c0.23.206.206a_dst-jpg_p206x206&_nc_cat=107&ccb=1-7&_nc_sid=da31f3&_nc_ohc=48oLeHCrZT4AX-kGk6s&_nc_ht=scontent-den4-1.xx&oh=00_AfCQQ3A6opo1xnGCheuGBeN0ayzE2xor3y_u2-VjQBPRig&oe=64E0D7A4&auto=format&fit=crop&w=738&q=80",
+    image: "Images/mattPic2.jpg",
     id: 3,
   },
   {
-    image: "Images/mattPic1.jpg",
+    image: "Images/mattpic3.jpg",
     id: 4,
   },
   {
-    image: "Images/mattPic2.jpg",
+    image: "Images/mattPic4.jpg",
     id: 5,
   },
   {
-    image: "Images/mattpic3.jpg",
-    id: 6,
-  },
-  {
-    image: "Images/mattPic4.jpg",
-    id: 7,
-  },
-  {
     image: "Images/mattPic5.jpg",
-    id: 8,
+    id: 6,
   },
 ];
