8d6705857c1998b2303fc56dcdab34c72775ee0e
Matthew Rowley <mattrowley10@gmail.com>
2023-09-18 10:12:34 -0600

    removed fb pic

--- src/Library/SliderData.js
+++ src/Library/SliderData.js
@@ -1,23 +1,18 @@
 export const SliderData = [
   {
-    image:
-      "https://scontent-den4-1.xx.fbcdn.net/v/t1.6435-9/134174951_3880489448636127_5629388319572094141_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=0JcilSE31y4AX8DbXn-&_nc_ht=scontent-den4-1.xx&oh=00_AfDbXsAPj8V6xaB1UefgI_ETJuMOGrO6pI2g1rTVMHcAdQ&oe=65036A49&auto=format&fit=crop&w=1170&q=80",
+    image: "Images/mattPic2.jpg",
     id: 1,
   },
   {
-    image: "Images/mattPic2.jpg",
+    image: "Images/mattpic3.jpg",
     id: 2,
   },
   {
-    image: "Images/mattpic3.jpg",
+    image: "Images/mattPic4.jpg",
     id: 3,
   },
   {
-    image: "Images/mattPic4.jpg",
-    id: 4,
-  },
-  {
     image: "Images/mattPic5.jpg",
-    id: 5,
+    id: 4,
   },
 ];
