38516671ba6e6b43fbd4569f9b9f8e68eea16be2
Matthew Rowley <mattrowley10@gmail.com>
2023-09-28 11:36:25 -0600

    pulled all but 1 pic

--- src/Library/SliderData.js
+++ src/Library/SliderData.js
@@ -3,16 +3,16 @@ export const SliderData = [
     image: "Images/mattpic3.jpg",
     id: 1,
   },
-  {
-    image: "Images/mattPic2.jpg",
-    id: 2,
-  },
-  {
-    image: "Images/mattPic4.jpg",
-    id: 3,
-  },
-  {
-    image: "Images/mattPic5.jpg",
-    id: 4,
-  },
+  // {
+  //   image: "Images/mattPic2.jpg",
+  //   id: 2,
+  // },
+  // {
+  //   image: "Images/mattPic4.jpg",
+  //   id: 3,
+  // },
+  // {
+  //   image: "Images/mattPic5.jpg",
+  //   id: 4,
+  // },
 ];
